96ac9daa908668720297eb5bcadc946c1c175379
Sumbati10 <sumbatilinda@gmail.com>
2022-10-10 04:52:48 -0700

    defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# linda shirasala <lindashirasala@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+     """Represent a rectangle."""
+     pass
