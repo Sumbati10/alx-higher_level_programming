c4246176328924057275b0012003a9a4d393d4e0
Sumbati10 <105505214+Sumbati10@users.noreply.github.com>
2022-12-03 11:41:59 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-""" script that lists all states from the database hbtn_0e_0_usa """
-from sys import argv
+
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
+"""
+
+
+import sys
 import MySQLdb
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8")
-    cur = conn.cursor()
-    try:
-        stmt = "SELECT * FROM states ORDER BY id ASC"
-        cur.execute(stmt)
-        rtn = cur.fetchall()
-    except MySQLdb.Error:
-        try:
-            rtn = ("MySQLdb Error")
-        except IndexError:
-            rtn = ("MySQLdb Error - IndexError")
-    for i in rtn:
-        print(i)
-    cur.close()
-    conn.close()
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
